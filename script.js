42b09794fd1f46faa84765692ab76d8f13153250
xodm <xodmg200@gmail.com>
2024-04-02 00:01:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,8 @@ var nodes = [
     .data(nodes)
     .enter().append("circle")
       .attr("class", "node")
-      .attr("r", 5);
+      .attr("r", 5)
+      .call(drag(simulation)); // Enable dragging
   
   // Update the positions of the nodes and links on each tick
   simulation.on("tick", function() {
@@ -52,3 +53,29 @@ var nodes = [
       .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; });
   });
+  
+  // Enable dragging behavior
+  function drag(simulation) {
+  
+    function dragstarted(event, d) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+  
+    function dragged(event, d) {
+      d.fx = event.x;
+      d.fy = event.y;
+    }
+  
+    function dragended(event, d) {
+      if (!event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+  
+    return d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+  }
